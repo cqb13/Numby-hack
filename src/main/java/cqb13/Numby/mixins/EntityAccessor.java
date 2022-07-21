663f597e3e167d2614839e32a3b9b25138779f0e
cqb13 <74616162+cqb13@users.noreply.github.com>
2022-07-21 05:18:41 +0300

    mixins and events for 1.4 update

---
+++ src/main/java/cqb13/Numby/mixins/EntityAccessor.java
@@ -0,0 +1,12 @@
+package cqb13.NumbyHack.mixins;
+
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+
+@Mixin(Entity.class)
+public interface EntityAccessor {
+    @Invoker("setFlag")
+    void invokeSetFlag(int index, boolean value);
+}
---
+++ src/main/java/cqb13/Numby/mixins/meteor/CrystalAuraAccessor.java
@@ -0,0 +1,15 @@
+package cqb13.NumbyHack.mixins.meteor;
+
+import meteordevelopment.meteorclient.systems.modules.combat.CrystalAura;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(CrystalAura.class)
+public interface CrystalAuraAccessor {
+    @Accessor("bestTarget")
+    PlayerEntity bestTarget();
+
+    @Accessor("bestTargetDamage")
+    double bestTargetDamage();
+}

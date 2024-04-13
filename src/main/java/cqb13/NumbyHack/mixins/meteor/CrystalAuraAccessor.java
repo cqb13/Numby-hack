dec712ba01338d63c6d9152cb2fc9e0a7fa15e4a
cqb13 <cqb13.main@gmail.com>
2024-04-13 13:11:58 -0400

    Fix crash on launch

--- src/main/java/cqb13/NumbyHack/mixins/meteor/CrystalAuraAccessor.java
+++ src/main/java/cqb13/NumbyHack/mixins/meteor/CrystalAuraAccessor.java
@@ -1,14 +1,14 @@
 package cqb13.NumbyHack.mixins.meteor;
 
 import meteordevelopment.meteorclient.systems.modules.combat.CrystalAura;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(CrystalAura.class)
 public interface CrystalAuraAccessor {
     @Accessor("bestTarget")
-    PlayerEntity bestTarget();
+    LivingEntity bestTarget();
 
     @Accessor(value = "bestTargetDamage", remap = false)
     double bestTargetDamage();

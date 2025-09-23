32ab4574a6295f61ded2fd56bc0b5bd00f3980a9
cqb13 <cqb13.main@gmail.com>
2025-09-22 21:56:26 -0400

    updated gradle stuff

---
+++ build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    id("fabric-loom") version "1.11-SNAPSHOT"
+}
+
+base {
+    archivesName = properties["archives_base_name"] as String
+    version = properties["mod_version"] as String
+    group = properties["maven_group"] as String
+}
+
+repositories {
+    maven {
+        name = "meteor-maven"
+        url = uri("https://maven.meteordev.org/releases")
+    }
+    maven {
+        name = "meteor-maven-snapshots"
+        url = uri("https://maven.meteordev.org/snapshots")
+    }
+}
+
+loom {
+    accessWidenerPath = file("src/main/resources/numby.accesswidener")
+}
+
+dependencies {
+    // Fabric
+    minecraft("com.mojang:minecraft:${properties["minecraft_version"] as String}")
+    mappings("net.fabricmc:yarn:${properties["yarn_mappings"] as String}:v2")
+    modImplementation("net.fabricmc:fabric-loader:${properties["loader_version"] as String}")
+
+    // Meteor
+    modImplementation("meteordevelopment:meteor-client:${properties["minecraft_version"] as String}-SNAPSHOT")
+}
+
+tasks {
+    processResources {
+        val propertyMap = mapOf(
+            "version" to project.version,
+            "mc_version" to project.property("minecraft_version"),
+        )
+
+        inputs.properties(propertyMap)
+
+        filteringCharset = "UTF-8"
+
+        filesMatching("fabric.mod.json") {
+            expand(propertyMap)
+        }
+    }
+
+    jar {
+        inputs.property("archivesName", project.base.archivesName.get())
+
+        from("LICENSE") {
+            rename { "${it}_${inputs.properties["archivesName"]}" }
+        }
+    }
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.release = 21
+        options.compilerArgs.add("-Xlint:deprecation")
+        options.compilerArgs.add("-Xlint:unchecked")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}

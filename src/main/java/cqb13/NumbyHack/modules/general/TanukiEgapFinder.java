fe7878dc179fc89ac7289aa6d6d2b7c649c4a686
cqb13 <cqb13.main@gmail.com>
2025-12-22 19:43:18 -0500

    code formatting

--- src/main/java/cqb13/NumbyHack/modules/general/TanukiEgapFinder.java
+++ src/main/java/cqb13/NumbyHack/modules/general/TanukiEgapFinder.java
@@ -3,12 +3,19 @@ package cqb13.NumbyHack.modules.general;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
 
 import cqb13.NumbyHack.NumbyHack;
 import meteordevelopment.meteorclient.events.world.ChunkDataEvent;
 import meteordevelopment.meteorclient.events.world.TickEvent;
-import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.DoubleSetting;
+import meteordevelopment.meteorclient.settings.IntSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.meteorclient.utils.player.ChatUtils;
@@ -35,59 +42,55 @@ public class TanukiEgapFinder extends Module {
     private final SettingGroup sgAutoSearch = settings.createGroup("Auto-Search");
 
     private final Setting<Boolean> playSound = sgDefault.add(new BoolSetting.Builder()
-        .name("play-sound")
-        .description("Plays a sound when you find an egap.")
-        .defaultValue(false)
-        .build());
+            .name("play-sound")
+            .description("Plays a sound when you find an egap.")
+            .defaultValue(false)
+            .build());
 
     private final Setting<Boolean> autoSearch = sgAutoSearch.add(new BoolSetting.Builder()
-        .name("auto-search")
-        .description("Teleports you to a new area to be scanned when the current area is already completed.")
-        .defaultValue(false)
-        .build());
+            .name("auto-search")
+            .description("Teleports you to a new area to be scanned when the current area is already completed.")
+            .defaultValue(false)
+            .build());
 
     private final Setting<Integer> renderDistance = sgAutoSearch.add(new IntSetting.Builder()
-        .name("render-distance")
-        .description("The render distance in your settings, this is used to calculate the position to tp to.")
-        .defaultValue(16)
-        .min(5)
-        .sliderMax(32)
-        .visible(autoSearch::get)
-        .build()
-    );
+            .name("render-distance")
+            .description("The render distance in your settings, this is used to calculate the position to tp to.")
+            .defaultValue(16)
+            .min(5)
+            .sliderMax(32)
+            .visible(autoSearch::get)
+            .build());
 
     private final Setting<Double> noChestTimeout = sgAutoSearch.add(new DoubleSetting.Builder()
-        .name("no-chest-timeout")
-        .description("Time in seconds where no chest is found and the area is deemed as looted.")
-        .defaultValue(2.5)
-        .min(1)
-        .sliderMax(10)
-        .visible(autoSearch::get)
-        .decimalPlaces(1)
-        .build()
-    );
+            .name("no-chest-timeout")
+            .description("Time in seconds where no chest is found and the area is deemed as looted.")
+            .defaultValue(2.5)
+            .min(1)
+            .sliderMax(10)
+            .visible(autoSearch::get)
+            .decimalPlaces(1)
+            .build());
 
     private final Setting<Double> chunkStableTime = sgAutoSearch.add(new DoubleSetting.Builder()
-        .name("chunk-stable-time")
-        .description("Time in seconds no new chunks are loading before moving on.")
-        .defaultValue(2.5)
-        .min(1)
-        .sliderMax(10)
-        .visible(autoSearch::get)
-        .decimalPlaces(1)
-        .build()
-    );
+            .name("chunk-stable-time")
+            .description("Time in seconds no new chunks are loading before moving on.")
+            .defaultValue(2.5)
+            .min(1)
+            .sliderMax(10)
+            .visible(autoSearch::get)
+            .decimalPlaces(1)
+            .build());
 
     private final Setting<Double> teleportCooldown = sgAutoSearch.add(new DoubleSetting.Builder()
-        .name("teleport-cooldown")
-        .description("Minimum time in seconds to wait after teleporting before moving again.")
-        .defaultValue(5)
-        .min(1)
-        .sliderMax(20)
-        .visible(autoSearch::get)
-        .decimalPlaces(1)
-        .build()
-    );
+            .name("teleport-cooldown")
+            .description("Minimum time in seconds to wait after teleporting before moving again.")
+            .defaultValue(5)
+            .min(1)
+            .sliderMax(20)
+            .visible(autoSearch::get)
+            .decimalPlaces(1)
+            .build());
 
     private enum ProcessState {
         IDLE,
@@ -109,7 +112,8 @@ public class TanukiEgapFinder extends Module {
 
     public TanukiEgapFinder() {
         super(NumbyHack.CATEGORY, "egap-finder",
-            "Finds Enchanted Golden Apples in chests and logs coordinates to " + OUTPUT_FILE_NAME + "-yourworldseed.txt");
+                "Finds Enchanted Golden Apples in chests and logs coordinates to " + OUTPUT_FILE_NAME
+                        + "-yourworldseed.txt");
     }
 
     @Override
@@ -131,7 +135,6 @@ public class TanukiEgapFinder extends Module {
             return;
         }
 
-
         ticksSinceLastChunkData++;
         ticksSinceTeleport++;
 
@@ -149,11 +152,11 @@ public class TanukiEgapFinder extends Module {
         }
 
         if (autoSearch.get()
-            && ticksWithoutChest >= noChestTimeout.get() * 20
-            && ticksSinceLastChunkData >= chunkStableTime.get() * 20
-            && ticksSinceTeleport >= teleportCooldown.get() * 20)
-        {
-            debug("Chunks stable (" + ticksSinceLastChunkData + " ticks) and minimum wait satisfied, moving to new area");
+                && ticksWithoutChest >= noChestTimeout.get() * 20
+                && ticksSinceLastChunkData >= chunkStableTime.get() * 20
+                && ticksSinceTeleport >= teleportCooldown.get() * 20) {
+            debug("Chunks stable (" + ticksSinceLastChunkData
+                    + " ticks) and minimum wait satisfied, moving to new area");
             tpToNewSearchArea();
             resetState();
             ticksWithoutChest = 0;
@@ -174,10 +177,8 @@ public class TanukiEgapFinder extends Module {
         String command = "/tp %d ~ %d";
 
         ChatUtils.info(Formatting.GREEN +
-            String.format("Teleporting to new search area with the center at (%d, ~, %d)!",
-                positionToTeleportTo.x(), positionToTeleportTo.z()
-            )
-        );
+                String.format("Teleporting to new search area with the center at (%d, ~, %d)!",
+                        positionToTeleportTo.x(), positionToTeleportTo.z()));
 
         ChatUtils.sendPlayerMsg(String.format(command, positionToTeleportTo.x(), positionToTeleportTo.z()));
         ticksSinceTeleport = 0;
@@ -228,7 +229,7 @@ public class TanukiEgapFinder extends Module {
 
         if (existingBlock != Blocks.COMPARATOR) {
             String command = String.format("/setblock %d %d %d minecraft:comparator[facing=east]",
-                comparatorPos.getX(), comparatorPos.getY(), comparatorPos.getZ());
+                    comparatorPos.getX(), comparatorPos.getY(), comparatorPos.getZ());
             ChatUtils.sendPlayerMsg(command);
         }
     }
@@ -239,7 +240,7 @@ public class TanukiEgapFinder extends Module {
 
         if (existingBlock != Blocks.ACACIA_LEAVES) {
             String command = String.format("/setblock %d %d %d minecraft:acacia_leaves",
-                leavesPos.getX(), leavesPos.getY(), leavesPos.getZ());
+                    leavesPos.getX(), leavesPos.getY(), leavesPos.getZ());
             ChatUtils.sendPlayerMsg(command);
         }
 
@@ -260,11 +261,10 @@ public class TanukiEgapFinder extends Module {
         }
 
         String command = String.format(
-            "/execute if data block %d %d %d Items[{id:\"minecraft:enchanted_golden_apple\"}] " +
-                "as @p run setblock %d %d %d minecraft:diamond_block",
-            currentChestPos.getX(), currentChestPos.getY(), currentChestPos.getZ(),
-            currentChestPos.getX(), currentChestPos.getY(), currentChestPos.getZ()
-        );
+                "/execute if data block %d %d %d Items[{id:\"minecraft:enchanted_golden_apple\"}] " +
+                        "as @p run setblock %d %d %d minecraft:diamond_block",
+                currentChestPos.getX(), currentChestPos.getY(), currentChestPos.getZ(),
+                currentChestPos.getX(), currentChestPos.getY(), currentChestPos.getZ());
 
         ChatUtils.sendPlayerMsg(command);
 
@@ -282,7 +282,7 @@ public class TanukiEgapFinder extends Module {
             handleEgapFound(lastCheckedChestPos);
         } else {
             String command = String.format("/setblock %d %d %d minecraft:air",
-                lastCheckedChestPos.getX(), lastCheckedChestPos.getY(), lastCheckedChestPos.getZ());
+                    lastCheckedChestPos.getX(), lastCheckedChestPos.getY(), lastCheckedChestPos.getZ());
             ChatUtils.sendPlayerMsg(command);
         }
     }
@@ -363,7 +363,8 @@ public class TanukiEgapFinder extends Module {
             neighbors.add(new XZPos(position.x, position.z + blockOffset));
 
             for (XZPos neighbor : neighbors) {
-                if (visited.contains(neighbor) || queued.contains(neighbor)) continue;
+                if (visited.contains(neighbor) || queued.contains(neighbor))
+                    continue;
 
                 queue.add(neighbor);
                 queued.add(neighbor);
@@ -371,7 +372,8 @@ public class TanukiEgapFinder extends Module {
         }
     }
 
-    record XZPos(int x, int z) {}
+    record XZPos(int x, int z) {
+    }
 
     private String getOutputFileName() {
         if (mc.world == null) {
